061b889398bb0efaeac1215f566a3a5831935c9a
Caleb Cole <calebrylancole@gmail.com>
2026-06-13 02:19:39 -0400

    feat(!): Change to Btrfs root
    Btrfs blank root ends up being a much simpler option than using a
    tmpfs. Also /home directories are moved out in plans of making them
    part of Guix Home.

--- config/system/taurinus-alpha.scm
+++ config/system/taurinus-alpha.scm
@@ -15,27 +15,42 @@
   #:use-module (base-system)
   #:export (taurinus-alpha-record))
 
-;; The path of a directory should be the same on btrfs as with tmpfs, just with /mnt/persist/ prepended
-(define (persistent-subvolume path dependency)
-  (file-system
-   (device (string-append "/mnt/persist/" path))
-   (mount-point (string-append "/" path))
-   (type "none")
-   (flags '(bind-mount))
-   (create-mount-point? #t)
-   (needed-for-boot? #t)
-   (dependencies (list dependency))))
+;; Helper function to make adding bind-mounts simple.
+(define (persistent-subvolume path)
+  (let ((persist-path (string-append "/persist/" path)))
+    (mkdir-p persist-path)		; Ensures the paths are present, and generates them if they are not.
+    (file-system
+     (device persist-path)
+     (mount-point (string-append "/" path))
+     (type "none")
+     (flags '(bind-mount))
+     (create-mount-point? #t)
+     (needed-for-boot? #t)
+     (dependencies 
+      (list (car (filter (lambda (fs) 
+                           (string=? (file-system-mount-point fs) "/persist"))
+                         %base-file-systems)))))))
 
-;; Only for /home/ volumes. %my-user is exported by (base-system).
-(define (persistent-home-subvolume path dependency)
-  (file-system
-   (device (string-append "/mnt/persist/home/user/" path))
-   (mount-point (string-append "/home/" %my-user "/" path))
-   (type "none")
-   (flags '(bind-mount))
-   (create-mount-point? #t)
-   (needed-for-boot? #t)
-   (dependencies (list dependency))))
+(define %btrfs-rollback-hook
+  #~(lambda (mount-args)
+      (let ((target "/mnt-parent"))
+	(mkdir target)
+	;; Mount the top-level Btrfs filesystem (subvol ID 5) to /mnt-parent
+	(if (zero? (system* "mount" "-o" "subvol=/" "/dev/mapper/Guix" target))
+            (begin
+              (display "Wiping ephemeral root file system...\n")
+              ;; Delete the mutated @root subvolume
+              (system* "btrfs" "subvolume" "delete" 
+                       (string-append target "/@root"))
+              ;; Recreate @root from the pristine @root_blank template
+              (system* "btrfs" "subvolume" "snapshot" 
+                       (string-append target "/@root_blank") 
+                       (string-append target "/@root"))
+              ;; Unmount and cleanup
+              (system* "umount" target)
+              (rmdir target)
+              (display "Rollback complete!\n"))
+            (display "WARNING: Failed to mount Btrfs root for rollback!\n")))))
 
 ;; Exported for use in generating an image for system installation.
 (define taurinus-alpha-record
@@ -43,109 +58,101 @@
    (inherit base-system)
    (host-name "taurinus-alpha")
 
+   (initrd (lambda (file-systems . rest)
+             (apply base-initrd file-systems
+                    #:extra-modules '("btrfs") ; Make sure the kernel can read Btrfs early on
+                    rest)))
+
    (mapped-devices (list (mapped-device
 			  (source "/dev/nvme0n1p2")
-			  (target "persist")
+			  (target "Guix")
 			  (type luks-device-mapping))))
 
    (file-systems
-    (let ((persist (file-system
-  		    (mount-point "/mnt/persist")
-  		    (device "/dev/mapper/persist")
-  		    (type "btrfs")
-  		    (needed-for-boot? #t)
-  		    (flags '(no-atime no-suid))
-  		    (options "subvol=@persist,compress=zstd,space_cache=v2")
-  		    (dependencies mapped-devices))))
-      (append (list
-  	       ;; Boot partition.
-  	       (file-system
-  		(mount-point "/boot/efi")
-  		(device (file-system-label "boot"))
-  		(type "vfat")
-		(needed-for-boot? #t)
-		(flags '(no-exec))
-		(options "umask=0077"))
+    (append (list
+  	     ;; Boot partition.
+  	     (file-system
+  	      (mount-point "/boot/efi")
+  	      (device (file-system-label "BOOT"))
+  	      (type "vfat")
+	      (needed-for-boot? #t)
+	      (flags '(no-exec))
+	      (options "umask=0077"))
 
-  	       ;; Ram-based root filesystem enforces impermanence.
-  	       (file-system
-  		(mount-point "/")
-  		(device "none")
-  		(type "tmpfs")
-  		(check? #f)
-  		(flags '(no-dev no-atime no-suid))
-  		(options "size=25%,mode=0755"))
+	     ;; Root volume. Gets overwritten by blank root sub-volume.
+	     (file-system
+	      (mount-point "/")
+	      (device (uuid "ac384527-0f36-4850-ba65-657fef2d18fe"))
+	      (type "btrfs")
+	      (needed-for-boot? #t)
+	      (flags '(no-dev no-atime no-suid))
+	      (options "subvol=@root,compress=zstd,space_cache=v2")
+	      (dependencies mapped-devices))
 
-  	       ;; Mounting for the store.
-  	       (file-system
-  		(mount-point "/gnu/store")
-  		(device "/dev/mapper/persist")
-  		(type "btrfs")
-  		(needed-for-boot? #t)
-  		(flags '(no-atime read-only))
-  		(options "subvol=@store,compress=zstd,space_cache=v2")
-  		(dependencies mapped-devices))
+  	     ;; Mounting for the store.
+  	     (file-system
+  	      (mount-point "/gnu")
+  	      (device (uuid "ac384527-0f36-4850-ba65-657fef2d18fe"))
+  	      (type "btrfs")
+  	      (needed-for-boot? #t)
+  	      (flags '(no-atime))
+  	      (options "subvol=@store,compress=zstd,space_cache=v2")
+  	      (dependencies mapped-devices))
 
-  	       ;; Mounting for all persistent data.
-  	       persist)
+	     ;; Persistence sub-volume.
+	     (file-system
+	      (mount-point "/persist")
+	      (device (uuid "ac384527-0f36-4850-ba65-657fef2d18fe"))
+	      (type "btrfs")
+	      (needed-for-boot? #t)
+	      (flags '(no-atime no-suid))
+	      (options "subvol=@persist,compress=zstd,space_cache=v2")
+	      (dependencies mapped-devices)))
 
-	      ;;; Bind-mounts		
-	      ;; System-mounts. "/mnt/persist/" is already prepended to volumes on btrfs.
-	      (map (lambda (paths)
-		     (persistent-subvolume paths persist))
-		   (list "etc/guix"	   ; Guix Daemon
-			 "var/guix"	   ; Guix Core
-			 "var/lib"	   ; Libraries
-			 "var/log"	   ; Logs
-			 "etc/NetworkManager/system-connections" ; NetworkManager Connections
-			 "etc/ssh"	     ; SSH Host Keys
-			 "etc/cups"	     ; CUPS Printing
-			 "etc/snapper/configs" ; Snapper Configurations
-			 "etc/default/snapper"
-			 ))
+	    ;; Bind-mounts.
+	    (map (lambda (paths)
+		   (persistent-subvolume paths))
+		 (list "etc/guix"	   ; Guix Daemon
+		       "var/guix"	   ; Guix Core
+		       "var/lib"	   ; Libraries
+		       "var/log"	   ; Logs
+		       "etc/NetworkManager/system-connections" ; NetworkManager Connections
+		       "etc/ssh"	     ; SSH Host Keys
+		       "etc/cups"	     ; CUPS Printing
+		       "etc/snapper/configs" ; Snapper Configurations
+		       "etc/default/snapper"
+		       ))
+	    %base-file-systems)
 
-  	      ;; Home-mounts
-	      (map (lambda (paths)
-		     (persistent-home-subvolume paths persist))
-		   (list "Projects"
-			 "Documents"
-			 "Pictures"
-			 "Music"
-			 "Videos"
-			 "Templates"
-			 ".ssh"
-			 ".gnupg"
-			 ".mozilla"		 ; Firefox
-			 ".tor project"		 ; Tor
-			 ".local/state/shephard" ; Shepherd Logs
-			 ".local/share/direnv"   ; Direnv
-			 ".local/share/Trash"	 ; XDG trash dir
-			 ".config/librewolf"	 ; Librewolf Config (Extensions, etc.)
-			 ".config/emacs"	 ; Emacs Config
-			 ".cache/emacs"		 ; Emacs Cache
-			 ".cache/guix"		 ; Guix Cache
-			 ))
-	      (filter (lambda (fs)
-			(let ((mp (file-system-mount-point fs)))
-                          (not (member mp '("/" "/boot/efi")))))
-		      %base-file-systems))))
+    (swap-devices
+     (list (swap-space
+	    (target (file-system-label "swap")))))
+    
+    (services
+     (append (list (service tlp-service-type
+  			    (tlp-configuration
+  			     (cpu-boost-on-ac? #t)
+  			     (wifi-pwr-on-bat? #t)))
+		   (service fprintd-service-type))
+	     btrfs-service
 
-   (services
-    (append (list (service tlp-service-type
-  		      (tlp-configuration
-  		       (cpu-boost-on-ac? #t)
-  		       (wifi-pwr-on-bat? #t)))
-		  (service fprintd-service-type))
-	    btrfs-service
-	    (operating-system-user-services base-system)))
+	     (modify-services %base-services
+			      (initrd-service-type config =>
+						   (initrd-configuration
+						    (inherit config)
+						    (activation-hooks 
+						     (cons %btrfs-rollback-hook
+							   (initrd-configuration-activation-hooks config))))))
+	     
+	     (operating-system-user-services base-system)))
 
-   (packages
-    (append '()
-	    (list emacs-exwm
-		  snapper
-		  btrfs-progs)
-	    (operating-system-packages base-system)))))
+    (packages
+     (append '()
+	     (list emacs-exwm
+		   snapper
+		   btrfs-progs)
+	     (operating-system-packages base-system)))))
 
 
-;; Evaluates into the definition above for use in 'sudo guix system reconfigure'.
-taurinus-alpha-record
+  ;; Evaluates into the definition above for use in 'sudo guix system reconfigure'.
+  taurinus-alpha-record
